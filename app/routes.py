8a56f1769a8f12cdd98b151096676e8ed5497699
Палирус Артём Геннадьевич <palirus@selectel.locl>
2019-10-24 20:07:17 +0300

    new

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,8 @@ from flask_login import logout_user
 from flask_login import login_required
 from app.forms import RegistrationForm
 from app import db
+from datetime import datetime
+
 
 @app.route('/')
 @app.route('/index')
@@ -58,3 +60,10 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+    
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
